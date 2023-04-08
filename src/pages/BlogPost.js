56cfbcff114dd43895980af1b737b65cc42b3191
perry1224 <perry.w1224@gmail.com>
2023-04-08 18:41:31 -0400

    updated timezone

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -5,6 +5,11 @@ import '../index.css';
 import { Link } from 'react-router-dom';
 
 const Blog = ({content}) => {
+  const createdDate = new Date(content.created).toLocaleDateString('en-US', { 
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
   return (
     <>
       <Link className='blog-goBack' to='/'>
@@ -13,7 +18,7 @@ const Blog = ({content}) => {
       {content ? (
         <div className='blog-wrap'>
           <header>
-            <p className='blog-date'>Published {content.created}</p>
+            <p className='blog-date'>Published {createdDate}</p>
             <h1>{content.title}</h1>
             <div className='blog-subCategory'>
               
