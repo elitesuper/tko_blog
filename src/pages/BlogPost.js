ee7a4dd841ef3db19bdabb02a4a054dc9d1ab736
Perry <104090157+perry1224@users.noreply.github.com>
2023-04-09 16:45:20 -0400

    re uploaded files

---
+++ src/pages/BlogPost.js
@@ -0,0 +1,40 @@
+import {React} from 'react';
+import Chip from '../components/Chip';
+import EmptyList from '../components/EmptyList';
+import '../index.css';
+import { Link } from 'react-router-dom';
+
+const Blog = ({content}) => {
+  const createdDate = new Date(content.created).toLocaleDateString('en-US', { 
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+  return (
+    <>
+      <Link className='blog-goBack' to='/'>
+        <span> &#8592;</span> <span>Go Back</span>
+      </Link>
+      {content ? (
+        <div className='blog-wrap'>
+          <header>
+            <p className='blog-date'>Published {createdDate}</p>
+            <h1>{content.title}</h1>
+            <div className='blog-subCategory'>
+              
+                <div>
+                  <Chip label={content.tags[0].name} />
+                </div>
+              
+            </div>
+          </header>
+          <img src={content.featured_image} alt='cover'/>
+          <div className='blog-content' dangerouslySetInnerHTML={{__html: content.body}}></div>
+        </div>
+      ) : (
+        <EmptyList />
+      )}
+    </>
+  );
+};
+export default Blog;
