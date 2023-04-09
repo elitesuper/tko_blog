ce7d4d231c5105b9589ac7a7c25219df1e81f105
perry1224 <perry.w1224@gmail.com>
2023-04-09 14:02:38 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import '../index.css';
 const Header = () => (
   <header className='home-header'>
-    <h2>The TKO Blog</h2>
+    <h2>My Personal Blog</h2>
     <h1>
-      <span>"</span> Have a look<span>"</span>
+      <span>"</span> Take a look at my Content <span>"</span>
     </h1>
     <p>
       Read, enjoy <br /> and contribute.
