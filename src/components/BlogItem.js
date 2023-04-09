c5d4f1ce4eb1ddd7cfec41b518e44c675f44ea47
perry1224 <perry.w1224@gmail.com>
2023-04-09 11:00:20 -0400

    updated item card

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -6,7 +6,8 @@ import '../index.css';
 const BlogItem = ({blog, content}) => {
   return (
    <div className='blogItem-wrap' key={blog.title}>
-      <img className='blogItem-cover' src={blog.featured_image} alt='cover' />
+    <Link to ={`/blog/${blog.title}`} onClick={()=>{content(blog)}}>
+      <img className='blogItem-cover' src={blog.featured_image} alt='cover' /></Link>
       <Chip label={blog.tags[0].name} />
       <h3>{blog.title}</h3>
       <p className='blogItem-desc'>{blog.summary}</p>
--- src/components/EmptyList.js
+++ src/components/EmptyList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../index.css';
 const EmptyList = () => (
   <div className='emptyList-wrap'>
-    <img src='/assets/13525-empty.gif' alt='empty' />
+    <img src="..img/wondering.jpg" alt='empty' />
   </div>
 );
 export default EmptyList;
