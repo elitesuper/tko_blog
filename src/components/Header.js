47901a473520d41c2908fe40f7d12c4a85223155
Perry <104090157+perry1224@users.noreply.github.com>
2023-04-09 23:52:00 -0400

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css';
+const Header = () => (
+  <header className='home-header'>
+    <h2>The TKO Blog</h2>
+    <h1>
+      <i>Recommendations from our podcast</i>
+    </h1>
+    <p>
+      Food, destinations, entertainment, you name it
+    </p>
+  </header>
+);
+export default Header;
