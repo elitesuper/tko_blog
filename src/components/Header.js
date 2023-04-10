0186eaf598d16ac536d1c0d4d40bc91f0e8e8046
Perry Wong <perry.w1224@gmail.com>
2023-04-10 20:09:05 +0000

    Update file Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../index.css';
 const Header = () => (
   <header className='home-header'>
-    <h2>The TKO Blog</h2>
+    <h2>This is a test</h2>
     <h1>
       <i>Recommendations from our podcast</i>
     </h1>
