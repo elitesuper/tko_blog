5677f88f72eccb71bdd4efb0bb80d377c9f66775
Perry Wong <perry.w1224@gmail.com>
2023-04-10 21:30:16 +0000

    Update 2 files
    - /src/index.css
    - /src/components/Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../index.css';
 const Header = () => (
   <header className='home-header'>
+    <a href="http://tkorajio.com" class="home">Home</a>
     <h2>TKO Rajio</h2>
     <h1>
       <i>Recommendations from our episodes</i>
